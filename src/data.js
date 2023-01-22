f9de9163ddd58a3e3f36582185d3bacc77ca6c02
ROHITchin995 <rohitsure.9915@gmail.com>
2023-01-22 23:25:44 +0530

    app init

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export default [
+    {
+      id: 1,
+      title: 'Samsung Galaxy S22 Ultra',
+      price: 57999.00,
+      img:
+        '../images/galaxyS22Ultra.jpg',
+      amount: 1,
+    },
+    {
+      id: 2,
+      title: 'google pixel 6',
+      price: 57000.00,
+      img:
+        '../images/googlePixel6.jpg',
+      amount: 1,
+    },
+    {
+      id: 3,
+      title: 'REDMI Note 12',
+      price: 18499.00,
+      img:
+        '../images/redmiNote12.jpg',
+      amount: 1,
+    },
+  ];
